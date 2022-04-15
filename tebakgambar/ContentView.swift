b069cc728806db1bca21be887cdfef1a9ed189ae
Fathur Rohman <vatoer.ckplus@gmail.com>
2022-04-15 15:33:38 +0100

    Step 9 buat warna background gelap

--- tebakgambar/ContentView.swift
+++ tebakgambar/ContentView.swift
@@ -11,19 +11,23 @@ struct ContentView: View {
     var gambarBatik = ["geblekrenteng-kulonprogo" , "gentongan-madura" , "kawung-jateng" , "keraton-yogyakarta" , "megamendung-cirebot" , "parang-jawa" , "sedapur-magetan" , "simbut-banten" , "sogan-solo" , "tujuhrupa-pekalongan"]
     var jawabanBenar = Int.random(in: 0...2)
     var body: some View {
-        VStack(spacing: 30) {
-            VStack {
-                Text("Tap batik motif")
-                Text(gambarBatik[jawabanBenar])
-            }
-            ForEach(0..<3) { number in
-                Button {
-                    // flag was tapped
-                } label: {
-                    Image(gambarBatik[number])
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap batik motif")
+                    Text(gambarBatik[jawabanBenar])
+                }
+                ForEach(0..<3) { number in
+                    Button {
+                        // flag was tapped
+                    } label: {
+                        Image(gambarBatik[number])
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    }
                 }
             }
         }
