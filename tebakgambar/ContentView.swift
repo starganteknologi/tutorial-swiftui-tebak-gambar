37def3c2e73249600b626c622e4dd23be38b477d
Fathur Rohman <vatoer.ckplus@gmail.com>
2022-04-15 15:42:01 +0100

    Step 11 Merandom urutan gambar batik

--- tebakgambar/ContentView.swift
+++ tebakgambar/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    var gambarBatik = ["geblekrenteng-kulonprogo" , "gentongan-madura" , "kawung-jateng" , "keraton-yogyakarta" , "megamendung-cirebot" , "parang-jawa" , "sedapur-magetan" , "simbut-banten" , "sogan-solo" , "tujuhrupa-pekalongan"]
+    var gambarBatik = ["geblekrenteng-kulonprogo" , "gentongan-madura" , "kawung-jateng" , "keraton-yogyakarta" , "megamendung-cirebon" , "parang-jawa" , "sedapur-magetan" , "simbut-banten" , "sogan-solo" , "tujuhrupa-pekalongan"].shuffled()
     var jawabanBenar = Int.random(in: 0...2)
     var body: some View {
         ZStack {
