63838eadfcb215065e96887bc849557b9e67a742
Fathur Rohman <vatoer.ckplus@gmail.com>
2022-04-15 17:05:47 +0100

    Step 18.2 Experiment 2 dengan background

--- tebakgambar/ContentView.swift
+++ tebakgambar/ContentView.swift
@@ -15,9 +15,9 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             RadialGradient(stops: [
-                .init(color: .red, location: 0.3),
-                .init(color: .green, location: 0.3),
-            ], center: .top, startRadius: 200, endRadius: 700)
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+            ], center: .top, startRadius: 200, endRadius: 400)
                 .ignoresSafeArea()
             VStack(spacing: 30) {
                 VStack {
