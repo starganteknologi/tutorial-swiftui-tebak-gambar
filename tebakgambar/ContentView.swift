fb0aa2f457f2b57315f17d7c4bb1363a38434a1a
Fathur Rohman <vatoer.ckplus@gmail.com>
2022-04-15 15:01:19 +0100

    step 4 buat variabel jawabanBenar

--- tebakgambar/ContentView.swift
+++ tebakgambar/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var gambarBatik = ["geblekrenteng-kulonprogo" , "gentongan-madura" , "kawung-jateng" , "keraton-yogyakarta" , "megamendung-cirebot" , "parang-jawa" , "sedapur-magetan" , "simbut-banten" , "sogan-solo" , "tujuhrupa-pekalongan"]
+    var jawabanBenar = Int.random(in: 0...2)
     var body: some View {
         Image("kawung-jateng")
     }
